87f22c477c57cb6f41d4cdfecc564b35b670511b
Dan Musyimi <danmusyimi63@gmail.com>
2026-04-15 18:58:38 +0300

    fix database connection to Aiven

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,27 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'techstock_db');
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
-$conn->set_charset("utf8");
+
+$host = "mysql-e1ae416-danmusyimi63-bd3d.b.aivencloud.com";
+$port = 28610;
+$dbname = "defaultdb";
+$username = "avnadmin";
+$password = getenv("DB_PASSWORD");
+
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect(
+    $conn,
+    $host,
+    $username,
+    $password,
+    $dbname,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
 ?>
